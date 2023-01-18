a21ce745d7c9204f2b95265385bc9c386d0a8255
Dell <archimbaud.maxence@gmail.com>
2023-01-18 21:16:27 +0100

    ajout fonction repet

---
+++ src/main/java/com/mycompany/variables_java/Variables_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variables_java;
+
+/**
+ *
+ * @author Dell
+ */
+public class Variables_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
